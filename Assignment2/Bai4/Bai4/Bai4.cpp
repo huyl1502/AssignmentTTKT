99b07e661c1ef43bd515dc0213af9c4a007d79f4
huyl1502 <huyl150201@gmail.com>
2021-11-10 17:38:24 +0700

    up ass2 b4

--- Assignment2/Bai4/Bai4/Bai4.cpp
+++ Assignment2/Bai4/Bai4/Bai4.cpp
@@ -1,110 +1,137 @@
 #include<iostream>
-#include<string>
 
 using namespace std;
-string formatString(string input)
-{
-	string s = input;
-	while (s[0] == '0')
-		s.erase(0, 1);
-	return s;
-}
-int convertToInt(string s) {
-	int n = s.length();
-	int num1 = 0;
-	for (int i = n - 1; i >= 0; --i) {
-		num1 += (int)((s[i] - '0') * pow(2, n - i - 1));
+
+int toDec(int* inp, int len) {
+	int rs = 0;
+	for (int i = 0; i < len; i++) {
+		rs += inp[i] * (pow(2, len - i - 1));
 	}
-	return num1;
-}
-int max(int a, int b) {
-	return a > b ? a : b;
-}
-string AND(string a, string b) {
-	int i, j;
-	i = a.length() - 1;
-	j = b.length() - 1;
-	int n = max(i, j);
-	char* s = new char[n + 1];
-	while (i >= 0 || j >= 0) {
-		if (i >= 0 && j >= 0) s[n] = a[i] & b[j];
-		else if (i < 0) s[n] = b[j];
-		else if (j < 0) s[n] = a[i];
-		if (n == 0) break;
-		n--;
-		i--;
-		j--;
-	}
-	string rs = s;
-	delete[] s;
 	return rs;
 }
-//string OR(string a, string b) {
-//
-//}
-//string XOR(string a, string b) {
-//
-//}
-//string NOT(string a, string b) {
-//
-//}
-//string LEFT_SHIFT(string a, string b) {
-//
-//}
-//string RIGHT_SHIFT(string a, string b) {
-//
-//}
+
+void toBinary(unsigned int inp) {
+	int binaryNum[32];
+	int i = 0;
+	while (inp > 0) {
+		binaryNum[i] = inp % 2;
+		inp = inp / 2;
+		i++;
+	}
+	for (int j = i - 1; j >= 0; j--)
+		cout << binaryNum[j];
+}
+
+int* preProcess(string s, int len) {
+	int s_len = s.length();
+	int* rs = new int[len];
+
+	if (s_len == len) {
+		for (int i = 0; i < len; i++) {
+			if (s[i] == '0') rs[i] = 0;
+			else rs[i] = 1;
+		}
+		return rs;
+	}
+
+	for (int i = 0; i < len; i++) {
+		rs[i] = 0;
+	}
+	int s_i;
+	for (int i = len - 1; i >= len - s_len; i--) {
+		s_i = s_len - (len - i);
+		if (s[s_i] == '0') rs[i] = 0;
+		else rs[i] = 1;
+	}
+	return rs;
+}
+
+void AND(int* a, int* b, int len) {
+	for (int i = 0; i < len; i++) {
+		if (a[i] == 1 && b[i] == 1) cout << '1';
+		else cout << '0';
+	}
+	cout << endl;
+}
+
+void OR(int* a, int* b, int len) {
+	for (int i = 0; i < len; i++) {
+		if (a[i] == 0 && b[i] == 0) cout << '0';
+		else cout << '1';
+	}
+	cout << endl;
+}
+
+void XOR(int* a, int* b, int len) {
+	for (int i = 0; i < len; i++) {
+		if (a[i] == b[i]) cout << '0';
+		else cout << '1';
+	}
+	cout << endl;
+}
+
+void NOT(int* a, int a_len, int len) {
+	cout << "~a = ";
+	if (a_len == len) {
+		for (int i = 0; i < a_len; i++) {
+			cout << 1 - a[i];
+		}
+	}
+	else {
+		for (int i = len - a_len; i < len; i++) {
+			cout << 1 - a[i];
+		}
+	}
+	cout << endl;
+}
+
+void LEFT_SHIFT(int* a, int* b, int len) {
+	int db = toDec(b, len);
+	int da = toDec(a, len);
+	int dc = da << db;
+	toBinary(dc);
+	cout << endl;
+}
+
+void RIGHT_SHIFT(int* a, int* b, int len) {
+	int db = toDec(b, len);
+	int da = toDec(a, len);
+	int dc = da >> db;
+	toBinary(dc);
+	cout << endl;
+}
 
 int main() {
-	string a, b;
-	char opt;
-	string c;
+	string c, sa, sb, bopt;
+	int* a; int* b;
+	int length;
 
-	while (true) {
-		//cout << ": ";
-		cout << "Nhap a = "; cin >> a;
-		cout << "Nhap b = "; cin >> b;
-		cout << "Bitwise operator('<' : shift left, '>' : shift right) = "; cin >> opt;
+	do {
+		cout << "-----------------------" << endl;
+		cout << "c. Continue" << endl;
+		cout << "e. Exit" << endl;
+		cout << "Enter: "; cin >> c;
+		if (c == "exit" || c == "quit" || c == "done") break;
+		
+		cout << "Input: " << endl;
+		cout << "a = "; cin >> sa;
+		cout << "b = "; cin >> sb;
+		cout << "bitwise operator = "; cin >> bopt;
 
+		length = max(sa.length(), sb.length());
+		int* a = preProcess(sa, length);
+		int* b = preProcess(sb, length);
 
-
-		switch (opt)
-		{
-		case '&':
-			cout << AND(a, b) << endl;
-			break;
-			/*case '|':
-				cout << AND(a, b) << endl;
-				break;
-			case '^':
-				cout << AND(a, b) << endl;
-				break;
-			case '~':
-			{
-				cout << AND(a, b) << endl;
-				break;
-			}
-
-			case '>':
-				cout << AND(a, b) << endl;
-				break;
-			case '<':
-				cout << AND(a, b) << endl;
-				break;*/
-		default:
-			cout << "khong hop le" << endl;
-			break;
+		if (bopt == "&") AND(a, b, length);
+		else if (bopt == "|") OR(a, b, length);
+		else if (bopt == "^") XOR(a, b, length);
+		else if (bopt == "~") {
+			NOT(a, sa.length(), length);
+			NOT(b, sb.length(), length);
 		}
-		cout << "Press Enter to continue, 'exit' or 'quit' or 'done' to Exit" << endl;
-		cin.ignore();
-		getline(std::cin, c);
-		if (c == "exit" || c == "quit" || c == "done") exit(0);
-		else {
-			c.clear();
-			continue;
-		}
-
-
-		return 0;
-	}
+		else if (bopt == "<<") LEFT_SHIFT(a, b, length);
+		else if (bopt == ">>") RIGHT_SHIFT(a, b, length);
+	} while (c != "e");
+	
+	return 0;
 }
