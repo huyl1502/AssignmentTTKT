1df1d99014569172e1417a5d15dea850f9988578
huyl1502 <huyl150201@gmail.com>
2021-11-10 15:47:07 +0700

    up ass2 b2

--- Assignment2/Bai2/Bai2/Bai2.cpp
+++ Assignment2/Bai2/Bai2/Bai2.cpp
@@ -1,13 +1,73 @@
-#include <iostream>
+#include<iostream>
+#include<string>
+#include<iomanip>
 
 using namespace std;
-
-int cong(int a, int b) {
-    return a + b;
+template<class T>
+T cong(const T& a, const T& b) {
+	return a + b;
+}
+template<class T>
+T tru(const T& a, const T& b) {
+	return a - b;
+}
+template<class T>
+T nhan(const T& a, const T& b) {
+	return a * b;
+}
+template<class T>
+void chia(const T& a, const T& b) {
+	if (b != 0)
+		cout << (float)a / b << setprecision(7) << fixed << endl;
+	else cout << "khong chia duoc" << endl;
+}
+int mod(const int& a, const int& b) {
+	return a % b;
 }
 
 int main()
 {
-    
-    return 0;
+	int a, b;
+	char opt;
+	char c;
+
+	do {
+		cout << "-----------------------" << endl;
+		cout << "c. Continue" << endl;
+		cout << "e. Exit" << endl;
+		cout << "Chon: "; cin >> c;
+
+		if (c == 'c') {
+			cout << "Nhap phep tinh: ";
+			cin >> a >> opt >> b;
+
+			switch (opt)
+			{
+			case '+':
+				cout << cong(a, b) << endl;
+				break;
+			case '-':
+				cout << tru(a, b) << endl;
+				break;
+			case '*':
+				cout << nhan(a, b) << endl;
+				break;
+			case '/':
+			{
+				chia(a, b);
+				break;
+			}
+
+			case '%':
+				cout << mod(a, b) << endl;
+				break;
+
+			default:
+				cout << "khong hop le" << endl;
+				break;
+			}
+		}
+	} while (c != 'e');
+
+	return 0;
 }
--- Assignment2/Bai4/Bai4/Bai4.cpp
+++ Assignment2/Bai4/Bai4/Bai4.cpp
@@ -1,8 +1,110 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 
 using namespace std;
-
-int main()
+string formatString(string input)
 {
-    return 0;
+	string s = input;
+	while (s[0] == '0')
+		s.erase(0, 1);
+	return s;
+}
+int convertToInt(string s) {
+	int n = s.length();
+	int num1 = 0;
+	for (int i = n - 1; i >= 0; --i) {
+		num1 += (int)((s[i] - '0') * pow(2, n - i - 1));
+	}
+	return num1;
+}
+int max(int a, int b) {
+	return a > b ? a : b;
+}
+string AND(string a, string b) {
+	int i, j;
+	i = a.length() - 1;
+	j = b.length() - 1;
+	int n = max(i, j);
+	char* s = new char[n + 1];
+	while (i >= 0 || j >= 0) {
+		if (i >= 0 && j >= 0) s[n] = a[i] & b[j];
+		else if (i < 0) s[n] = b[j];
+		else if (j < 0) s[n] = a[i];
+		if (n == 0) break;
+		n--;
+		i--;
+		j--;
+	}
+	string rs = s;
+	delete[] s;
+	return rs;
+}
+//string OR(string a, string b) {
+//
+//}
+//string XOR(string a, string b) {
+//
+//}
+//string NOT(string a, string b) {
+//
+//}
+//string LEFT_SHIFT(string a, string b) {
+//
+//}
+//string RIGHT_SHIFT(string a, string b) {
+//
+//}
+
+int main() {
+	string a, b;
+	char opt;
+	string c;
+
+	while (true) {
+		//cout << ": ";
+		cout << "Nhap a = "; cin >> a;
+		cout << "Nhap b = "; cin >> b;
+		cout << "Bitwise operator('<' : shift left, '>' : shift right) = "; cin >> opt;
+
+
+
+		switch (opt)
+		{
+		case '&':
+			cout << AND(a, b) << endl;
+			break;
+			/*case '|':
+				cout << AND(a, b) << endl;
+				break;
+			case '^':
+				cout << AND(a, b) << endl;
+				break;
+			case '~':
+			{
+				cout << AND(a, b) << endl;
+				break;
+			}
+
+			case '>':
+				cout << AND(a, b) << endl;
+				break;
+			case '<':
+				cout << AND(a, b) << endl;
+				break;*/
+		default:
+			cout << "khong hop le" << endl;
+			break;
+		}
+		cout << "Press Enter to continue, 'exit' or 'quit' or 'done' to Exit" << endl;
+		cin.ignore();
+		getline(std::cin, c);
+		if (c == "exit" || c == "quit" || c == "done") exit(0);
+		else {
+			c.clear();
+			continue;
+		}
+
+
+		return 0;
+	}
 }
